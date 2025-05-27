a71e7e7bff074513bd1d2ac452a67f2854393594
Hirpara <harshilkumar.hirpara@ontario.ca>
2025-05-26 23:33:26 -0400

    gzip compression fix3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -164,8 +164,9 @@ public class Main {
                               GZIPOutputStream gzip = new GZIPOutputStream(compressedData);
                               gzip.write(content.getBytes(StandardCharsets.UTF_8));
                               gzip.close();
+                              byte[] compressed = compressedData.toByteArray();
                               int compressedDataLength = compressedData.toString().length();
-                              String encodingResponseMessage = "HTTP/1.1 "+ STATUS_200_OK + "\r\nContent-Encoding: gzip"+  "\r\nContent-Type: " + TEXT_PLAIN_CONTENT_TYPE + "\r\nContent-Length: " + compressedDataLength + "\r\n\r\n" + compressedData;
+                              String encodingResponseMessage = "HTTP/1.1 "+ STATUS_200_OK + "\r\nContent-Encoding: gzip"+  "\r\nContent-Type: " + TEXT_PLAIN_CONTENT_TYPE + "\r\nContent-Length: " + compressedDataLength + "\r\n\r\n" + Arrays.toString(compressed);
                               writer.write(encodingResponseMessage.getBytes());
                               break;
                           }
